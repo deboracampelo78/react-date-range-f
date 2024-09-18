71670eec4ce7763e53e6b8dc444496abbfec2cec
Debora Campelo <debora.campelo@benner.com.br>
2024-09-18 16:11:52 -0300

    Criação da Biblioteca

--- src/defaultRanges.js
+++ src/defaultRanges.js
@@ -43,14 +43,14 @@ export function createStaticRanges(ranges) {
 
 export const defaultStaticRanges = createStaticRanges([
   {
-    label: 'Today',
+    label: 'Hoje',
     range: () => ({
       startDate: defineds.startOfToday,
       endDate: defineds.endOfToday,
     }),
   },
   {
-    label: 'Yesterday',
+    label: 'Ontem',
     range: () => ({
       startDate: defineds.startOfYesterday,
       endDate: defineds.endOfYesterday,
@@ -58,28 +58,28 @@ export const defaultStaticRanges = createStaticRanges([
   },
 
   {
-    label: 'This Week',
+    label: 'Semana Atual',
     range: () => ({
       startDate: defineds.startOfWeek,
       endDate: defineds.endOfWeek,
     }),
   },
   {
-    label: 'Last Week',
+    label: 'Semana Passada',
     range: () => ({
       startDate: defineds.startOfLastWeek,
       endDate: defineds.endOfLastWeek,
     }),
   },
   {
-    label: 'This Month',
+    label: 'Mês Atual',
     range: () => ({
       startDate: defineds.startOfMonth,
       endDate: defineds.endOfMonth,
     }),
   },
   {
-    label: 'Last Month',
+    label: 'Mês Passado',
     range: () => ({
       startDate: defineds.startOfLastMonth,
       endDate: defineds.endOfLastMonth,
@@ -89,7 +89,7 @@ export const defaultStaticRanges = createStaticRanges([
 
 export const defaultInputRanges = [
   {
-    label: 'days up to today',
+    label: 'Qt Dias anteriores à Hoje',
     range(value) {
       return {
         startDate: addDays(defineds.startOfToday, (Math.max(Number(value), 1) - 1) * -1),
@@ -103,7 +103,7 @@ export const defaultInputRanges = [
     },
   },
   {
-    label: 'days starting today',
+    label: 'Qt Dias após Hoje',
     range(value) {
       const today = new Date();
       return {
